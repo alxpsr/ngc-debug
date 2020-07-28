b51f7ce5c9e545162948a9678371d21d82141121
alxpsr <alxpsr@mail.ru>
2020-07-28 11:23:02 +0500

    update-for-v9

--- background.js
+++ background.js
@@ -3,16 +3,28 @@ var panels = chrome.devtools.panels;
 panels.elements.createSidebarPane(
     '@Component',
     function(sidebar) {        
-        function updatePane() {
-            function getNode() {
-                return typeof ng === 'undefined' || !ng.probe($0) ? { "message": "content is not readable" } : {
-                    "Component": ng.probe($0).componentInstance,
-                    "Parent": ng.probe($0).parent ? ng.probe($0).parent.componentInstance : 'root level',
-                    "_debug": ng.probe($0)
+        function getNodeV2() {            
+            if (typeof ng !== 'undefined') {
+                if (ng.probe && ng.probe($0)) {
+                    console.log('legacy');
+                    return {
+                        "Component": ng.probe($0).componentInstance,
+                        "Parent": ng.probe($0).parent ? ng.probe($0).parent.componentInstance : 'root level',
+                        "_debug": ng.probe($0)
+                    }
+                } else if (!ng.probe && ng.getComponent($0)) {
+                    return {
+                        "Component": ng.getComponent($0),
+                    }
                 }
             }
 
-            sidebar.setExpression("(" + getNode.toString() + ")()");
+            
+            return { "message": "content is not readable" };
+        }
+        
+        function updatePane() {
+            sidebar.setExpression("(" + getNodeV2.toString() + ")()");
         }
 
         updatePane();
