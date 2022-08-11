425afa3817b3228481dc5957eb8ed8a44a24cb1b
alxpsr <alxpsr@mail.ru>
2022-08-11 12:40:55 +0500

    Made recursively printing of debug

--- background.js
+++ background.js
@@ -2,33 +2,61 @@ var panels = chrome.devtools.panels;
 
 panels.elements.createSidebarPane(
     '@Component',
-    function(sidebar) {        
-        function getNodeV2() {            
+    function (sidebar) {
+        function getNodeV2() {
+            /**
+             * 
+             * @param {String} entryPoint name of component
+             * @returns { component: ComponentRef, parent: { component: ComponentRef } }
+             */
+            function getClosestComponent(entryPoint) {
+                const ref = ng.getComponent(entryPoint);
+
+                if (!ref) {
+                    const parent = entryPoint.parentElement;
+
+                    if (parent) {
+                        return getClosestComponent(parent);
+                    } else {
+                        return;
+                    }
+
+                }
+
+                const parent = ng.getHostElement(ref).parentElement;
+
+                return {
+                    Component: ref,
+                    Parent: parent ? getClosestComponent(parent) : null
+                };
+            }
+            
             if (typeof ng !== 'undefined') {
                 if (ng.probe && ng.probe($0)) {
-                    
+
                     return {
                         "Component": ng.probe($0).componentInstance,
                         "Parent": ng.probe($0).parent ? ng.probe($0).parent.componentInstance : 'root level',
                         "_debug": ng.probe($0)
                     }
-                } else if (!ng.probe && ng.getComponent($0)) {
+                } else {
                     return {
-                        "Component": ng.getComponent($0),
+                        // "Component": ng.getComponent($0),
+                        "Component": getClosestComponent($0),
                     }
                 }
             }
 
-            
-            return { "message": "content is not readable" };
+
+            // return { "message": "content is not readable" };
         }
-        
+
         function updatePane() {
             sidebar.setExpression("(" + getNodeV2.toString() + ")()");
         }
 
         updatePane();
-        
+
         panels.elements.onSelectionChanged.addListener(updatePane);
     }
 )
